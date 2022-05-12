29bdd593672e543ca2acd58c602cdf08fc0c92fd
BuckLuckDuck <nikitos80011@gmail.com>
2022-05-12 17:30:40 +0700

    0.2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,6 +43,7 @@
             this.btn_next_round = new System.Windows.Forms.Button();
             this.lbl_rounds = new System.Windows.Forms.Label();
             this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.grid_match_results)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.grid_team_lambda)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.grid_team_points)).BeginInit();
@@ -53,9 +54,10 @@
             this.btn_exit.BackColor = System.Drawing.Color.IndianRed;
             this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btn_exit.Location = new System.Drawing.Point(1240, 12);
+            this.btn_exit.Location = new System.Drawing.Point(955, 414);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_exit.Name = "btn_exit";
-            this.btn_exit.Size = new System.Drawing.Size(45, 45);
+            this.btn_exit.Size = new System.Drawing.Size(60, 55);
             this.btn_exit.TabIndex = 101;
             this.btn_exit.Text = "X";
             this.btn_exit.UseVisualStyleBackColor = false;
@@ -69,13 +71,12 @@
             this.grid_match_results.AllowUserToResizeRows = false;
             this.grid_match_results.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.grid_match_results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grid_match_results.ColumnHeadersVisible = false;
-            this.grid_match_results.Location = new System.Drawing.Point(18, 12);
+            this.grid_match_results.Location = new System.Drawing.Point(24, 15);
+            this.grid_match_results.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grid_match_results.Name = "grid_match_results";
             this.grid_match_results.ReadOnly = true;
-            this.grid_match_results.RowHeadersVisible = false;
             this.grid_match_results.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.grid_match_results.Size = new System.Drawing.Size(436, 298);
+            this.grid_match_results.Size = new System.Drawing.Size(581, 367);
             this.grid_match_results.TabIndex = 102;
             // 
             // grid_team_lambda
@@ -89,23 +90,26 @@
             this.grid_team_lambda.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.clmn_team,
             this.clmn});
-            this.grid_team_lambda.Location = new System.Drawing.Point(1037, 12);
+            this.grid_team_lambda.Location = new System.Drawing.Point(1093, 15);
+            this.grid_team_lambda.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grid_team_lambda.Name = "grid_team_lambda";
             this.grid_team_lambda.ReadOnly = true;
             this.grid_team_lambda.RowHeadersVisible = false;
             this.grid_team_lambda.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.grid_team_lambda.Size = new System.Drawing.Size(180, 298);
+            this.grid_team_lambda.Size = new System.Drawing.Size(240, 367);
             this.grid_team_lambda.TabIndex = 103;
             // 
             // clmn_team
             // 
             this.clmn_team.HeaderText = "Team";
+            this.clmn_team.MinimumWidth = 6;
             this.clmn_team.Name = "clmn_team";
             this.clmn_team.ReadOnly = true;
             // 
             // clmn
             // 
             this.clmn.HeaderText = "Lambda";
+            this.clmn.MinimumWidth = 6;
             this.clmn.Name = "clmn";
             this.clmn.ReadOnly = true;
             // 
@@ -123,50 +127,57 @@
             this.clmn_win,
             this.clmn_draw,
             this.clmn_lose});
-            this.grid_team_points.Location = new System.Drawing.Point(575, 12);
+            this.grid_team_points.Location = new System.Drawing.Point(613, 15);
+            this.grid_team_points.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grid_team_points.Name = "grid_team_points";
             this.grid_team_points.ReadOnly = true;
             this.grid_team_points.RowHeadersVisible = false;
             this.grid_team_points.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.grid_team_points.Size = new System.Drawing.Size(354, 298);
+            this.grid_team_points.Size = new System.Drawing.Size(472, 367);
             this.grid_team_points.TabIndex = 104;
             // 
             // dataGridViewTextBoxColumn1
             // 
             this.dataGridViewTextBoxColumn1.HeaderText = "Team";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
             this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
             this.dataGridViewTextBoxColumn1.ReadOnly = true;
             // 
             // clmn_points
             // 
             this.clmn_points.HeaderText = "Points";
+            this.clmn_points.MinimumWidth = 6;
             this.clmn_points.Name = "clmn_points";
             this.clmn_points.ReadOnly = true;
             // 
             // clmn_win
             // 
             this.clmn_win.HeaderText = "Win";
+            this.clmn_win.MinimumWidth = 6;
             this.clmn_win.Name = "clmn_win";
             this.clmn_win.ReadOnly = true;
             // 
             // clmn_draw
             // 
             this.clmn_draw.HeaderText = "Draw";
+            this.clmn_draw.MinimumWidth = 6;
             this.clmn_draw.Name = "clmn_draw";
             this.clmn_draw.ReadOnly = true;
             // 
             // clmn_lose
             // 
             this.clmn_lose.HeaderText = "Lose";
+            this.clmn_lose.MinimumWidth = 6;
             this.clmn_lose.Name = "clmn_lose";
             this.clmn_lose.ReadOnly = true;
             // 
             // btn_rnd_teams
             // 
             this.btn_rnd_teams.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btn_rnd_teams.Location = new System.Drawing.Point(1037, 326);
+            this.btn_rnd_teams.Location = new System.Drawing.Point(1093, 390);
+            this.btn_rnd_teams.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_rnd_teams.Name = "btn_rnd_teams";
-            this.btn_rnd_teams.Size = new System.Drawing.Size(180, 43);
+            this.btn_rnd_teams.Size = new System.Drawing.Size(240, 53);
             this.btn_rnd_teams.TabIndex = 105;
             this.btn_rnd_teams.Text = "Random teams";
             this.btn_rnd_teams.UseVisualStyleBackColor = true;
@@ -176,20 +187,24 @@
             // 
             this.btn_next_round.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.btn_next_round.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btn_next_round.Location = new System.Drawing.Point(1127, 497);
+            this.btn_next_round.Location = new System.Drawing.Point(1094, 506);
+            this.btn_next_round.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_next_round.Name = "btn_next_round";
-            this.btn_next_round.Size = new System.Drawing.Size(158, 58);
+            this.btn_next_round.Size = new System.Drawing.Size(211, 71);
             this.btn_next_round.TabIndex = 106;
             this.btn_next_round.Text = "Next round";
             this.btn_next_round.UseVisualStyleBackColor = false;
+            this.btn_next_round.Visible = false;
+            this.btn_next_round.Click += new System.EventHandler(this.btn_next_round_Click);
             // 
             // lbl_rounds
             // 
             this.lbl_rounds.AutoSize = true;
             this.lbl_rounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lbl_rounds.Location = new System.Drawing.Point(1156, 474);
+            this.lbl_rounds.Location = new System.Drawing.Point(1132, 477);
+            this.lbl_rounds.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_rounds.Name = "lbl_rounds";
-            this.lbl_rounds.Size = new System.Drawing.Size(104, 20);
+            this.lbl_rounds.Size = new System.Drawing.Size(129, 25);
             this.lbl_rounds.TabIndex = 107;
             this.lbl_rounds.Text = "Round: 0 / 14";
             // 
@@ -198,18 +213,33 @@
             this.btn_reset.BackColor = System.Drawing.Color.Salmon;
             this.btn_reset.Enabled = false;
             this.btn_reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btn_reset.Location = new System.Drawing.Point(1036, 520);
+            this.btn_reset.Location = new System.Drawing.Point(972, 534);
+            this.btn_reset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_reset.Name = "btn_reset";
-            this.btn_reset.Size = new System.Drawing.Size(85, 35);
+            this.btn_reset.Size = new System.Drawing.Size(113, 43);
             this.btn_reset.TabIndex = 108;
             this.btn_reset.Text = "Reset";
             this.btn_reset.UseVisualStyleBackColor = false;
             // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_start.Location = new System.Drawing.Point(1093, 507);
+            this.btn_start.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(211, 71);
+            this.btn_start.TabIndex = 109;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1296, 566);
+            this.ClientSize = new System.Drawing.Size(1353, 612);
+            this.Controls.Add(this.btn_start);
             this.Controls.Add(this.btn_reset);
             this.Controls.Add(this.lbl_rounds);
             this.Controls.Add(this.btn_next_round);
@@ -219,6 +249,7 @@
             this.Controls.Add(this.grid_match_results);
             this.Controls.Add(this.btn_exit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -247,6 +278,7 @@
         private System.Windows.Forms.Button btn_next_round;
         private System.Windows.Forms.Label lbl_rounds;
         private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_start;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,12 +13,48 @@ namespace Simulation.Lab._10
     public partial class Form1 : Form
     {
         string[] teams = new string[8] {"RUS", "ARG", "ESP", "GER", "ENG", "POR", "FRA", "WAL"};
+        int[] teams_id = new int[8] { 0, 1, 2, 3, 4, 5, 6, 7 };
         float[] team_lambda = new float[8];
-
+        int[,] team_matches = new int[8, 8];
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void main_diagonale_setter()
+        {
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                    if (i == j) team_matches[i, j] = 1;
+        }
+
+        private int[] teams_next_round_matches()
+        {
+            int[] teams_id_next = new int[8];
+            int k = 0;
+            int[] teams_temp = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (teams_temp[i] == 0)
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (team_matches[i,j] != 1 && teams_temp[j] == 0)
+                        {
+                            teams_id_next[k] = i;
+                            teams_id_next[k+1] = j;
+                            k += 2;
+
+                            team_matches[i, j] = 1;
+
+                            teams_temp[i] = 1;
+                            teams_temp[j] = 1;
+                            break;
+                        }
+                    }
+            }
+            return teams_id_next;
         }
 
         private void btn_exit_Click(object sender, EventArgs e)
@@ -35,6 +71,32 @@ namespace Simulation.Lab._10
                 team_lambda[i] = (float)rnd.Next(201) / 100;
                 grid_team_lambda.Rows.Add(teams[i], team_lambda[i]);
             }
+        }
+
+        private void btn_next_round_Click(object sender, EventArgs e)
+        {
+            int[] next_round = new int[8];
+            next_round = teams_next_round_matches();
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            main_diagonale_setter();
+            for (int i = 0; i < teams.Length; i++)
+            {
+                grid_match_results.Columns.Add(new DataGridViewColumn { HeaderText = teams[i]});
+            }
+            // grid_match_results.Rows.Add("", teams[0], teams[1], teams[2], teams[3], teams[4], teams[5], teams[6], teams[7]);
+
+            for (int i = 0; i < teams.Length; i++)
+            {
+                //grid_match_results.Rows.Add(" ", " ", " ", " ", " ", " ", " ", " ");
+                grid_match_results.Rows[i].HeaderCell = new DataGridViewRowHeaderCell { Value = teams[i] };
+            }
+
+            btn_start.Visible = false;
+            btn_next_round.Visible = true;
+            btn_rnd_teams.Enabled = false;
         }
     }
 }
